844f85b27233d776fd6f95a3fc42c139ef0e1bce
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-09-23 10:25:43 +0200

    feat: added user model

--- app/core/db.py
+++ app/core/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import NullPool
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from app.core.settings import settings
 
@@ -7,3 +7,7 @@ engine = create_async_engine(settings.postgres_dsn, poolclass=NullPool)
 
 # Create the async session
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+class ModelBase(DeclarativeBase):
+    pass
---
+++ app/jwt_auth/user_model.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import String, func
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.db import ModelBase  # you might want to replace this line
+
+
+class User(AsyncAttrs, ModelBase):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    email: Mapped[str] = mapped_column(
+        String(320)
+    )  # 320 is max email size according to RFC 3696
+    hashed_password: Mapped[str] = mapped_column(String(200))
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
